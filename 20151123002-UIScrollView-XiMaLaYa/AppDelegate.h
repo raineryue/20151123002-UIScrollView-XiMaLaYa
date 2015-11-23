65b4714004b5e66a66623f7012e3d140a085eaf6
raineryue <it_jinlei.yue@163.com>
2015-11-23 22:21:31 +0800

    Initial Commit

---
+++ 20151123002-UIScrollView-XiMaLaYa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20151123002-UIScrollView-XiMaLaYa
+//
+//  Created by Rainer on 15/11/23.
+//  Copyright © 2015年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 20151123002-UIScrollView-XiMaLaYa/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  20151123002-UIScrollView-XiMaLaYa
+//
+//  Created by Rainer on 15/11/23.
+//  Copyright © 2015年 Rainer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
