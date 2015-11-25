a2a7e72016a54f3756bcb59a8d32aee748639831
raineryue <jinlei_yue@sina.com>
2015-11-25 17:31:53 +0800

    使用AutoLayout完成界面布局并实现功能
    使用AutoLayout完成界面布局并实现功能

--- 20151123002-UIScrollView-XiMaLaYa/ViewController.m
+++ 20151123002-UIScrollView-XiMaLaYa/ViewController.m
@@ -10,14 +10,47 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIScrollView *ximalayScrollView;
+@property (weak, nonatomic) IBOutlet UIView *lastSubView;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // 获取高度
+//    CGFloat scrollViewH = self.lastSubView.frame.size.height + 10;
+//    
+//
+//    self.ximalayScrollView.contentInset = UIEdgeInsetsMake(64, 0, 49, 0);
+//
+//    self.ximalayScrollView.contentSize = CGSizeMake(0, scrollViewH);
+//    
+//    self.ximalayScrollView.contentOffset = CGPointMake(0, -64);
+//    
+//    self.ximalayScrollView.showsHorizontalScrollIndicator = NO;
+//    self.ximalayScrollView.showsVerticalScrollIndicator = NO;
 }
+
+- (void)setXimalayScrollView:(UIScrollView *)ximalayScrollView {
+    _ximalayScrollView = ximalayScrollView;
+    
+    // 获取高度
+    CGFloat scrollViewH = self.lastSubView.frame.size.height + 10;
+    
+    
+    self.ximalayScrollView.contentInset = UIEdgeInsetsMake(64, 0, 49, 0);
+    
+    self.ximalayScrollView.contentSize = CGSizeMake(0, scrollViewH);
+    
+    self.ximalayScrollView.contentOffset = CGPointMake(0, -64);
+    
+    self.ximalayScrollView.showsHorizontalScrollIndicator = NO;
+    self.ximalayScrollView.showsVerticalScrollIndicator = NO;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
